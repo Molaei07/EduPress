76a3dcbf165107330b160b81b4267da4da57a7e0
Molaei <molaie.web.developer@gmail.com>
2024-06-20 12:55:11 +0100

    Add new fonts to tialwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'ExoSemiBold': '"ExoSemiBold"',
+      'JostMedium': '"JostMedium"',
+      'JostRegular': '"JostRegular"'
+    },
+    extend: {}
   },
   plugins: [],
 }
