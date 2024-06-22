f14a4d75e45ed63b8bc4ed1f4d6d9a7a0df3cce7
Molaei <molaie.web.developer@gmail.com>
2024-06-22 17:47:58 +0100

    Create feedbacks items

---
+++ src/components/gridViews/feedbacks/studentFeedbacks/studentFeedbacksItems/feedbackItems.jsx
@@ -0,0 +1,16 @@
+const CreateItemFeedbacks = () => {
+    return (
+        <div className="w-full lg:w-[22%] h-fit border-gray-300 border-solid border rounded-xl px-3 2xl:px-6 py-5 2xl:py-10 flex flex-wrap gap-y-2 2xl:gap-y-8">
+            <div className="w-8 h-8">
+                <img className="w-full h-full" src="./images/“.png" alt="Logo" />
+            </div>
+            <p className="font-JostRegular text-sm w-full">I must explain to you how all this mistaken . Tdea of denouncing pleasure and praising pain was born and I will give you a complete account of the system and expound</p>
+            <div className="w-full flex flex-wrap gap-y-2">
+                <h3 className="w-full font-ExoSemiBold text-base">Roe Smith</h3>
+                <span className="w-full text-sm font-JostRegular text-slate-400">Designer</span>
+            </div>
+        </div>
+    );
+}
+
+export default CreateItemFeedbacks;
