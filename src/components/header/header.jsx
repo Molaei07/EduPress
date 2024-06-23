e83b3179ca89e37288f5f47748cc9e54ec0792c4
Molaei <molaie.web.developer@gmail.com>
2024-06-23 13:06:38 +0100

    Add name application component to this component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,11 @@
 import { NavLink } from "react-router-dom";
+import ApplicationName from "../applicationName/applicationName";
 
 const Header = () => {
     return (
         <header className="w-full h-12 flex justify-center items-center px-4 sm:px-16 xl:px-48 shadow-md relative z-10">
             <div className="w-full max-w-screen-xl h-full flex flex-wrap lg:flex-nowrap justify-between items-center">
-                <div className="w-40 h-1/2 flex items-center justify-between gap-x-1">
-                    <img className="w-1/6 h-full" src="./images/Isolation_Mode.png" alt="Logo" />
-                    <span className="w-5/6 h-full text-2xl leading-6 font-ExoSemiBold">EduPress</span>
-                </div>
+                <ApplicationName />
                 <ul className="w-full order-last lg:order-none lg:w-4/5 h-full hidden lg:flex justify-center items-center leading-8 font-ExoSemiBold text-xs">
                     <NavLink className="Menuitems" to="/" style={{color: "#FF782D", backgroundColor: "#F5F5F5"}}>Home</NavLink>
                     <li className="Menuitems">Courses</li>
