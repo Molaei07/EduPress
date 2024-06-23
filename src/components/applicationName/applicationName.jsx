d34a64e34e074c75aa373f32bf4d91175d5cf01c
Molaei <molaie.web.developer@gmail.com>
2024-06-23 13:05:45 +0100

    create new component for name application

---
+++ src/components/applicationName/applicationName.jsx
@@ -0,0 +1,10 @@
+const ApplicationName = () => {
+    return (
+        <div className="w-40 h-1/2 flex items-center justify-between gap-x-1">
+            <img className="w-1/6 h-full" src="./images/Isolation_Mode.png" alt="Logo" />
+            <span className="w-5/6 h-full text-2xl leading-6 font-ExoSemiBold">EduPress</span>
+        </div>
+    );
+}
+
+export default ApplicationName;
