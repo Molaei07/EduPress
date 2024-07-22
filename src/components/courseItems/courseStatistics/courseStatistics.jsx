6412f8cc240b38d135f44f0b51577b14e89cc40b
Molaei <molaie.web.developer@gmail.com>
2024-07-22 17:57:49 +0100

    Add new props and change parent

--- src/components/courseItems/courseStatistics/courseStatistics.jsx
+++ src/components/courseItems/courseStatistics/courseStatistics.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
-const CourseStatistics = ({picture, name}) => {
+const CourseStatistics = ({ info, picture, name }) => {
     return (
-        <>
+        <li className={`${info ? 'flex' : 'hidden'} w-auto lg:w-auto items-center gap-x-1 lg:gap-x-2`}>
             <div className="w-2.5 md:w-3.5">
-                <img className="w-full h-full" src={"./images/" + picture} alt="Logo" />
+                <img className="w-full h-full" src={"../../../../public/images/" + picture} alt="Logo" />
             </div>
-            <span className="text-xs md:text-sm lg:text-base">{name}</span>
-        </>
+            <span className="text-xs md:text-sm lg:text-base dark:text-white font-JostRegular">{name}</span>
+        </li>
     );
 }
 
