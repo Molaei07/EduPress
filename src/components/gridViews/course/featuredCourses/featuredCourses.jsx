17bf5db4093030a195199e83b9cf7c61a5e4619f
Molaei <molaie.web.developer@gmail.com>
2024-07-22 18:11:48 +0100

    Set translate component

--- src/components/gridViews/course/featuredCourses/featuredCourses.jsx
+++ src/components/gridViews/course/featuredCourses/featuredCourses.jsx
@@ -1,35 +1,55 @@
-// import CreateItems from "./createItems";
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import MediaQuery from "react-responsive";
 import Carousel from "react-multi-carousel";
-import ButtonGroup from "../../../allSliderBtn/allSliderBtn";
-import CourseView2 from "../../../courseItems/view2/courseView2"
-import CourseView1 from "../../../courseItems/view1/courseView1";
-import Responsive from "../responsiveSlider/responsive";
+import "react-multi-carousel/lib/styles.css";
+import ButtonGroup from "../../../sliders/buttons/allSliderBtn";
+import CoursesResponsiveSlider from "../../../sliders/responsive/courses/coursesResponsiveSlider";
+import { ItemsView1 } from "../../../courseItems/createItem";
+import { useEffect, useState } from "react";
+import { Courses, CoursesFa } from "../../../../data/courses/courses.js";
+import CourseView2 from "../../../courseItems/view2/courseView2";
+import { useTranslation } from "react-multi-lang";
+import { useSearchParams } from "react-router-dom";
+import CopyButton from "../../../copyPart/copyButton.jsx";
+import { getClientY } from "../../../copyPart/copy.js";
 
-export const FeaturedCourses = () => {
+export const FeaturedCourses = ({ language }) => {
+    // lang === false => language is english => Courses
+    // lang === true => language is persian => CoursesFa
+    const [lang, setLang] = useState(false)
+    // change lang
+    useEffect(() => {
+        setLang(language)
+    }, [language])
+
+    const translate = useTranslation()
+
+    const [searchParams, setSearchParams] = useSearchParams({clientY: 0})
+
     return (
         <div className="w-full flex flex-wrap gap-y-8">
             <div className="w-full flex justify-between items-center">
                 <div>
-                    <h1 className="gridViewName">Featured courses</h1>
-                    <span className="gridViewDescription">Explore our Popular Courses</span>
+                    <div className="flex gap-x-2">
+                        <CopyButton getFunction={getClientY} set={setSearchParams} />
+                        <h1 className="gridViewName">{translate("courseItems.title")}</h1>
+                    </div>
+                    <span className="gridViewDescription">{translate("courseItems.description")}</span>
                 </div>
-                <button className="gridViewButton">All courses</button>
+                <button className="gridViewButton" onClick={() => { setLang(!lang) }}>{translate("courseItems.button")}</button>
             </div>
             <MediaQuery minWidth={768}>
-                <ul className="w-full flex justify-center flex-wrap gap-2 lg:gap-4">
-                    <CourseView1 tag={'Photography'} onwerCourse={"Determined-Poitras"} name={"Create an LMS Website with LearnPress"} time={2} students={156} date={""} offPrice={"29.0"} originalPrice={0} picture={"Image1.png"} description={''} />
-                    <CourseView1 tag={'Photography'} onwerCourse={"Determined-Poitras"} name={"Design a website with ThimPress"} time={2} students={156} date={""} offPrice={"59.0"} originalPrice={"49.0"} picture={"Rectangle131.png"} description={''} />
-                    <CourseView1 tag={'Photography'} onwerCourse={"Determined-Poitras"} name={"Create an LMS Website with LearnPress"} time={2} students={156} date={""} offPrice={"29.0"} originalPrice={0} picture={"Image3.png"} description={''} />
-                    <CourseView1 tag={'Photography'} onwerCourse={"Determined-Poitras"} name={"Create an LMS Website with LearnPress"} time={2} students={156} date={""} offPrice={"29.0"} originalPrice={0} picture={"Image(1).png"} description={''} />
-                    <CourseView1 tag={'Photography'} onwerCourse={"Determined-Poitras"} name={"Create an LMS Website with LearnPress"} time={2} students={156} date={""} offPrice={"29.0"} originalPrice={0} picture={"Rectangle 139(1).png"} description={''} />
-                    <CourseView1 tag={'Photography'} onwerCourse={"Determined-Poitras"} name={"Create an LMS Website with LearnPress"} time={2} students={156} date={""} offPrice={"29.0"} originalPrice={0} picture={"Image(2).png"} description={''} />
+                <ul className={`w-full flex justify-center flex-wrap gap-2 lg:gap-4`}>
+                    {
+                        lang === false ? <ItemsView1 data={Courses} language={lang} /> : <ItemsView1 data={CoursesFa} language={lang} />
+                    }
                 </ul>
             </MediaQuery>
             <MediaQuery maxWidth={768}>
                 <Carousel
                     swipeable={false}
-                    responsive={Responsive}
+                    responsive={CoursesResponsiveSlider}
                     ssr={true} // means to render carousel on server-side.
                     infinite={true}
                     keyBoardControl={true}
@@ -38,14 +58,17 @@ export const FeaturedCourses = () => {
                     containerClass="carousel-container left-0 sm:pb-10"
                     deviceType={"mobile"}
                     arrows={false}
-                    customButtonGroup={<ButtonGroup />}
+                    customButtonGroup={<ButtonGroup language={lang} />}
+                    itemClass={lang === false ? "" : "dirRTL"}
+                    rtl={lang === false ? false : true}
                 >
-                    <CourseView2 onwerCourse={"Determined-Poitras"} name={"Create an LMS Website with LearnPress"} time={2} students={156} offPrice={"29.0"} originalPrice={0} tag={"Photography"} picture={"Image1.png"} />
-                    <CourseView2 onwerCourse={"Determined-Poitras"} name={"Design a website with ThimPress"} time={2} students={156} offPrice={"59.0"} originalPrice={"49.0"} tag={"Photography"} picture={"Rectangle131.png"} />
-                    <CourseView2 onwerCourse={"Determined-Poitras"} name={"Create an LMS Website with LearnPress"} time={2} students={156} offPrice={"29.0"} originalPrice={0} tag={"Photography"} picture={"Image3.png"} />
-                    <CourseView2 onwerCourse={"Determined-Poitras"} name={"Create an LMS Website with LearnPress"} time={2} students={156} offPrice={"29.0"} originalPrice={0} tag={"Photography"} picture={"Image(1).png"} />
-                    <CourseView2 onwerCourse={"Determined-Poitras"} name={"Create an LMS Website with LearnPress"} time={2} students={156} offPrice={"29.0"} originalPrice={0} tag={"Photography"} picture={"Rectangle 139(1).png"} />
-                    <CourseView2 onwerCourse={"Determined-Poitras"} name={"Create an LMS Website with LearnPress"} time={2} students={156} offPrice={"29.0"} originalPrice={0} tag={"Photography"} picture={"Image(2).png"} />
+                    {
+                        lang === false ? Courses.map((item) => {
+                            return (<CourseView2 key={item.id} language={lang} id={item.id} onwerCourse={item.owner} name={item.name} time={item.time} students={item.students} offPrice={item.offPrice} originalPrice={item.originalPrice} tag={item.tag} picture={item.picture} />)
+                        }) : CoursesFa.map((item) => {
+                            return (<CourseView2 key={item.id} language={lang} id={item.id} onwerCourse={item.owner} name={item.name} time={item.time} students={item.students} offPrice={item.offPrice} originalPrice={item.originalPrice} tag={item.tag} picture={item.picture} />)
+                        })
+                    }
                 </Carousel>
             </MediaQuery>
         </div>
