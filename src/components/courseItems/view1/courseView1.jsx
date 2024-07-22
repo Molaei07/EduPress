701e9e118fa01be15dee0da0e5bc646fdc686b29
Molaei <molaie.web.developer@gmail.com>
2024-07-22 17:56:51 +0100

    Create two views for courses

--- src/components/courseItems/view1/courseView1.jsx
+++ src/components/courseItems/view1/courseView1.jsx
@@ -1,44 +1,34 @@
+import { useTranslation } from "react-multi-lang";
+import CourseStatistics from "../courseStatistics/courseStatistics";
+import ViewMoreBtn from "../viewMoreBtn";
+
 /* eslint-disable react/prop-types */
-const CourseView1 = ({ tag, onwerCourse, name, time, students, date, offPrice, originalPrice, picture, description }) => {
+const CourseView1 = ({ language, id, viewInfo, tag, onwerCourse, name, time, students, date, offPrice, originalPrice, picture, description }) => {
+    const translate = useTranslation()
     return (
-        <li className="h-auto w-[32%] border border-solid border-gray-200 rounded-2xl flex flex-wrap overflow-hidden hoverItems slowMotion group">
+        <div className={`${viewInfo === 'view1' ? 'w-[48%]' : 'w-[32%]'} pb-2 sm:pb-0 h-auto border border-solid dark:border-gray-400 border-gray-200 rounded sm:rounded-2xl flex flex-wrap overflow-hidden hoverItems slowMotion group`}>
             <div className="h-1/2 w-full relative">
                 <img className="w-full h-full" src={"./images/" + picture} alt="Logo" />
-                <div className={`${tag ? "block" : "hidden"} bg-black text-white text-sm lg:text-base font-JostMedium rounded-lg absolute left-2 lg:left-4 top-2 lg:top-4 p-1 lg:p-2`}>Photography</div>
+                <div className={`${tag ? "block" : "hidden"} bg-black text-white text-xs sm:text-sm font-JostMedium rounded-lg absolute ${language === false ? "left-2 lg:left-4 lg:text-base" : "right-2 lg:right-4 lg:text-xl"} top-2 lg:top-4 p-1 lg:p-2`}>{tag}</div>
             </div>
             <div className="w-full h-1/2 flex flex-wrap p-2 lg:p-4">
-                <span className={`${onwerCourse === '' ? "hidden" : "block"} text-xs lg:text-sm text-gray-500 font-JostRegular py-0.5 lg:py-2`}>by <span className="text-black">{onwerCourse}</span></span>
-                <h1 className="w-full font-ExoSemiBold text-sm lg:text-xl slowMotion group-hover:text-orange-500">{name}</h1>
+                <span className={`${onwerCourse === '' ? "hidden" : "block"} text-[9px] sm:text-xs lg:text-sm dark:text-gray-400 text-gray-500 font-JostRegular py-0.5 lg:py-2`}>{translate("courseItems.by")} <span className="text-black dark:text-white">{onwerCourse}</span></span>
+                <h1 className="w-full font-ExoSemiBold text-xs dark:text-white sm:text-sm lg:text-xl slowMotion group-hover:text-orange-500">{name}</h1>
                 <div className="w-full py-1 lg:py-4 flex items-center gap-x-4">
-                    <div className={`${time === '' ? "hidden" : "flex"} items-center gap-x-2`}>
-                        <div className="w-3.5 h-3.5">
-                            <img className="w-full h-full" src="./images/Group(7).png" alt="Logo" />
-                        </div>
-                        <span className="text-xs text-gray-700 font-JostRegular">{time}Weeks</span>
-                    </div>
-                    <div className={`${students === '' ? "hidden" : "flex"} items-center gap-x-2`}>
-                        <div className="w-4 h-3">
-                            <img className="w-full h-full" src="./images/Vector.png" alt="Logo" />
-                        </div>
-                        <span className="text-xs text-gray-700 font-JostRegular">{students} Students</span>
-                    </div>
-                    <div className={`${date === '' ? "hidden" : "flex"} items-center gap-x-2`}>
-                        <div className="w-3.5 h-3.5">
-                            <img className="w-full h-full" src="./images/date.png" alt="Logo" />
-                        </div>
-                        <span className="font-JostRegular text-xs lg:text-sm text-slate-400">{date}</span>
-                    </div>
+                    <CourseStatistics info={time} picture={"Group(7).png"} name={`${time} ${translate("courseItems.weeks")}`} />
+                    <CourseStatistics info={students} picture={"Vector.png"} name={`${students} ${translate("courseItems.students")}`} />
+                    <CourseStatistics info={date} picture={"date.png"} name={date} />
                 </div>
                 <div className={`${originalPrice === '' ? "hidden" : "flex"} w-full justify-between border-t border-solid border-gray-200 lg:py-3`}>
                     <div className="w-fit flex items-end gap-x-2">
-                        <span className="text-gray-500 text-sm line-through font-JostRegular">${offPrice}</span>
-                        <span className={`${originalPrice == 0 ? "text-green-500" : "text-red-500"} font-JostRegular text-sm`}>{originalPrice == 0 ? "Free" : originalPrice}</span>
+                        <span className="text-gray-500 text-xs sm:text-sm line-through font-JostRegular">${offPrice}</span>
+                        <span className={`${originalPrice == 0 ? "text-green-500" : "text-red-500"} font-JostRegular text-xs sm:text-sm`}>{originalPrice == 0 ? `${translate("courseItems.Free")}` : originalPrice}</span>
                     </div>
-                    <button className="font-JostMedium text-xs lg:text-sm">View More</button>
+                    <ViewMoreBtn id={id} />
                 </div>
                 <p className={`${description === '' ? "hidden" : "block"} font-JostRegular text-slate-400 text-xs lg:text-sm`}>{description}</p>
             </div>
-        </li>
+        </div>
     );
 }
 
--- src/components/courseItems/view2/courseView2.jsx
+++ src/components/courseItems/view2/courseView2.jsx
@@ -1,41 +1,34 @@
 /* eslint-disable react/prop-types */
+import { useTranslation } from "react-multi-lang";
 import CourseStatistics from "../courseStatistics/courseStatistics";
+import ViewMoreBtn from "../viewMoreBtn";
 
-const CourseView2 = ({ tag, onwerCourse, name, time, students, level, lesson, date, offPrice, originalPrice, picture, description }) => {
+const CourseView2 = ({ language, id, tag, onwerCourse, name, time, students, level, lesson, date, offPrice, originalPrice, picture, description }) => {
+    const translate = useTranslation()
     return (
-        <div className="w-full h-full lg:h-64 rounded-2xl border border-gray-200 border-solid flex overflow-hidden hoverItems slowMotion group">
-            <div className="w-2/5 h-full">
+        <div className="w-full h-auto lg:h-64 rounded-2xl border dark:border-gray-400 border-gray-200 border-solid flex overflow-hidden hoverItems slowMotion group">
+            <div className="w-2/5">
                 <img className="w-full h-full" src={"./images/" + picture} alt="Logo" />
-                <div className={`${tag ? "block" : "hidden"} bg-black text-white text-xs sm:text-sm lg:text-base font-JostMedium rounded-lg absolute left-2 lg:left-4 top-2 lg:top-4 p-1 lg:p-2`}>{tag}</div>
+                <div className={`${tag ? "block" : "hidden"} bg-black text-white text-xs sm:text-sm lg:text-base font-JostMedium rounded-lg absolute ${language === false ? "left-2 lg:left-4" : "right-2 lg:right-4"} top-2 lg:top-4 p-1 lg:p-2`}>{tag}</div>
             </div>
             <div className="p-2 md:p-4 w-3/5 h-full flex flex-wrap gap-y-4 md:gap-y-10 lg:gap-y-16">
                 <div className="w-full h-fit flex gap-y-2 md:gap-y-3 flex-wrap">
-                    <span className={`${onwerCourse ? 'block' : 'hidden'} w-full text-xs md:text-sm font-JostRegular text-gray-400`}>by <span className="font-JostMedium text-black">{onwerCourse}</span></span>
-                    <h1 className="font-ExoSemiBold text-sm sm:text-base md:text-xl lg:text-2xl w-full">{name}</h1>
-                    <ul className="w-full h-auto lg:h-6 flex flex-wrap lg:flex-nowrap lg:gap-x-10 items-center text-gray-500 font-JostRegular">
-                        <li className={`${time ? 'flex' : 'hidden'} w-1/2 lg:w-auto items-center gap-x-1 lg:gap-x-2`}>
-                            <CourseStatistics picture={'Group(7).png'} name={`${time} Week`} />
-                        </li>
-                        <li className={`${students ? 'flex' : 'hidden'} w-1/2 lg:w-auto items-center gap-x-1 lg:gap-x-2`}>
-                            <CourseStatistics picture={'Vector.png'} name={`${students} Students`} />
-                        </li>
-                        <li className={`${level ? 'flex' : 'hidden'} w-1/2 lg:w-auto items-center gap-x-1 lg:gap-x-2`}>
-                            <CourseStatistics picture={'level.png'} name={"All levels"} />
-                        </li>
-                        <li className={`${lesson ? 'flex' : 'hidden'} w-1/2 lg:w-auto items-center gap-x-1 lg:gap-x-2`}>
-                            <CourseStatistics picture={'lessons.png'} name={`${time} Lessons`} />
-                        </li>
-                        <li className={`${date ? 'flex' : 'hidden'} w-1/2 lg:w-auto items-center gap-x-1 lg:gap-x-2`}>
-                            <CourseStatistics picture={'date.png'} name={date} />
-                        </li>
+                    <span className={`${onwerCourse ? 'block' : 'hidden'} w-full text-xs md:text-sm font-JostRegular text-gray-400`}>{translate("courseItems.by")} <span className="font-JostMedium dark:text-white text-black">{onwerCourse}</span></span>
+                    <h1 className="font-ExoSemiBold text-sm sm:text-base group-hover:text-orange-500 md:text-xl lg:text-2xl w-full dark:text-white">{name}</h1>
+                    <ul className="w-full h-auto lg:h-6 flex flex-wrap lg:flex-nowrap gap-2 items-center text-gray-500 font-JostRegular">
+                        <CourseStatistics info={time} picture={'Group(7).png'} name={`${time} ${translate("courseItems.weeks")}`} />
+                        <CourseStatistics info={students} picture={'Vector.png'} name={`${students} ${translate("courseItems.students")}`} />
+                        <CourseStatistics info={level} picture={'level.png'} name={`${level} ${translate("courseItems.levels")}`} />
+                        <CourseStatistics info={lesson} picture={'lessons.png'} name={`${time} ${translate("courseItems.lessons")}`} />
+                        <CourseStatistics info={date} picture={'date.png'} name={date} />
                     </ul>
                 </div>
                 <div className={`${originalPrice === '' ? "hidden" : "flex"} w-full justify-between items-center border-t border-solid border-gray-200`}>
                     <div className="w-fit flex items-end gap-x-2">
                         <span className="text-gray-500 text-xs md:text-sm lg:text-base line-through font-JostRegular">${offPrice}</span>
-                        <span className={`${originalPrice == 0 ? "text-green-500" : "text-red-500"} font-JostRegular text-xs md:text-sm lg:text-base`}>{originalPrice == 0 ? "Free" : originalPrice}</span>
+                        <span className={`${originalPrice == 0 ? "text-green-500" : "text-red-500"} font-JostRegular text-xs md:text-sm lg:text-base`}>{originalPrice == 0 ? `${translate("courseItems.Free")}` : originalPrice}</span>
                     </div>
-                    <button className="font-JostMedium text-xs md:text-sm lg:text-base">View More</button>
+                    <ViewMoreBtn id={id} />
                 </div>
                 <p className={`${description === '' ? "hidden" : "block"} font-JostRegular text-slate-400 text-xs lg:text-sm`}>{description}</p>
             </div>
