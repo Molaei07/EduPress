4f24b348c9cc3bcf36cfee4c1b354fb5fdccf64b
Molaei <molaie.web.developer@gmail.com>
2024-07-22 18:10:21 +0100

    Responsive slider about feedbacks part

---
+++ src/components/sliders/responsive/feedbacksResponsiveSlider/feedbacksResponsiveSlider.jsx
@@ -0,0 +1,14 @@
+const FeedbacksResponsiveSlider = {
+    tablet: {
+        breakpoint: { max: 1024, min: 640 },
+        items: 2,
+        slidesToSlide: 1 // optional, default to 1.
+    },
+    mobile: {
+        breakpoint: { max: 640, min: 0 },
+        items: 1,
+        slidesToSlide: 1 // optional, default to 1.
+    }
+};
+
+export default FeedbacksResponsiveSlider;
