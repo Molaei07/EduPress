28d71185e10c070ba9dd04d813dfef032daae1fa
Molaei <molaie.web.developer@gmail.com>
2024-07-22 17:53:39 +0100

    Change LOGO

--- src/components/applicationName/applicationName.jsx
+++ src/components/applicationName/applicationName.jsx
@@ -1,8 +1,13 @@
+import AppLogo from '../../assets/images/Isolation_Mode.png'
+import { useTranslation } from 'react-multi-lang';
+
 const ApplicationName = () => {
+    const translate = useTranslation()
+
     return (
         <div className="w-40 h-1/2 flex items-center justify-between gap-x-1">
-            <img className="w-1/6 h-full" src="./images/Isolation_Mode.png" alt="Logo" />
-            <span className="w-5/6 h-full text-2xl leading-6 font-ExoSemiBold">EduPress</span>
+            <img className="w-1/6 h-full" src={AppLogo} alt="Logo" />
+            <span className="w-5/6 h-full text-2xl leading-6 font-ExoSemiBold dark:text-white">{translate("applicationName")}</span>
         </div>
     );
 }
