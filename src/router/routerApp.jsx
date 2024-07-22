f3a91f2abf49189390a3745b9731858bbc9b4689
Molaei <molaie.web.developer@gmail.com>
2024-07-22 18:00:50 +0100

    Change router and add new page

--- src/router/routerApp.jsx
+++ src/router/routerApp.jsx
@@ -1,15 +1,47 @@
-import { Routes, Route } from "react-router-dom";
+import { Navigate, useRoutes } from "react-router-dom";
+// Layout
 import Layout from "../layout/layout";
-import { Home } from "../layout/homePage/home";
+// Pages
+import Home from "../pages/homePage/home"
+import AllCoursePage from "../pages/coursesPage/allCoursePage";
+import CourseSingle from "../pages/coursesSinglePage/courseSingle";
+// Tabs in course single
+import Overview from "../components/tabCourseSingle/tabContent/overview";
+import Curriculum from "../components/tabCourseSingle/tabContent/curriculum";
+import Instructor from "../components/tabCourseSingle/tabContent/instructor";
+import Faq from "../components/tabCourseSingle/tabContent/faqs";
+import Reviews from "../components/tabCourseSingle/tabContent/reviews";
 
 const RouterApp = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-            </Route>
-        </Routes>
-    );
+    const router = useRoutes([
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                { index: true, element: <Home />, },
+                { path: "/home", element: <Navigate to={"/"} /> },
+                {
+                    path: "/courses",
+                    children: [
+                        { index: true, element: <AllCoursePage /> },
+                        {
+                            path: ":id",
+                            element: <CourseSingle />,
+                            children: [
+                                { path: "overview", element: <Overview /> },
+                                { path: "curriculum", element: <Curriculum /> },
+                                { path: "instructor", element: <Instructor /> },
+                                { path: "faqs", element: <Faq /> },
+                                { path: "reviews", element: <Reviews /> }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ])
+
+    return router
 }
 
 export default RouterApp;
