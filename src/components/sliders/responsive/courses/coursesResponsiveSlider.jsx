84aec45def18f464f12b4c424c84462d680ca2f5
Molaei <molaie.web.developer@gmail.com>
2024-07-22 18:16:17 +0100

    Courses grid view responsive for slider

---
+++ src/components/sliders/responsive/courses/coursesResponsiveSlider.jsx
@@ -0,0 +1,9 @@
+const CoursesResponsiveSlider = {
+    mobile: {
+        breakpoint: { max: 1024, min: 0 },
+        items: 1,
+        slidesToSlide: 1 // optional, default to 1.
+    },
+};
+
+export default CoursesResponsiveSlider;
