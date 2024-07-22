9aaf4c89bf629ef34fc148b8769bd3ab7af42ebb
Molaei <molaie.web.developer@gmail.com>
2024-07-22 18:13:46 +0100

    Set translate component

--- src/components/gridViews/feedbacks/studentFeedbacks/studentFeedbacksItems/feedbackItems.jsx
+++ src/components/gridViews/feedbacks/studentFeedbacks/studentFeedbacksItems/feedbackItems.jsx
@@ -1,13 +1,17 @@
+import { useTranslation } from "react-multi-lang";
+
 const CreateItemFeedbacks = () => {
+    const translate = useTranslation()
+
     return (
-        <div className="w-full lg:w-[22%] h-fit border-gray-300 border-solid border rounded-xl px-3 2xl:px-6 py-5 2xl:py-10 flex flex-wrap gap-y-2 2xl:gap-y-8">
+        <div className="w-full lg:w-[22%] h-fit dark:border-gray-500 border-gray-300 border-solid border rounded-xl px-3 2xl:px-6 py-5 2xl:py-10 flex flex-wrap gap-y-2 2xl:gap-y-8">
             <div className="w-8 h-8">
                 <img className="w-full h-full" src="./images/“.png" alt="Logo" />
             </div>
-            <p className="font-JostRegular text-sm w-full">I must explain to you how all this mistaken . Tdea of denouncing pleasure and praising pain was born and I will give you a complete account of the system and expound</p>
+            <p className="font-JostRegular text-sm w-full dark:text-white">{translate("gridViews.feedbacks.feedbackText")}</p>
             <div className="w-full flex flex-wrap gap-y-2">
-                <h3 className="w-full font-ExoSemiBold text-base">Roe Smith</h3>
-                <span className="w-full text-sm font-JostRegular text-slate-400">Designer</span>
+                <h3 className="w-full font-ExoSemiBold text-base dark:text-white">{translate("gridViews.feedbacks.userName")}</h3>
+                <span className="w-full text-sm font-JostRegular text-slate-400">{translate("gridViews.feedbacks.side")}</span>
             </div>
         </div>
     );
