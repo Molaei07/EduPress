a3757c35f026c2ebd30f36c5102259aa4e2a5572
Molaei <molaie.web.developer@gmail.com>
2024-07-22 18:06:07 +0100

    Add darkmode btn and translate btn to layout

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,13 +1,29 @@
-import Header from "../components/header/header";
-import Footer from "../components/footer/footer";
+import Header from "./header/header";
+import Footer from "./footer/footer";
 import { Outlet } from "react-router-dom";
+import DarkBtn from "./darkModeBtn/darkBtn";
+import Translate from "../translate/translate.jsx";
+import { setDefaultLanguage, setDefaultTranslations } from 'react-multi-lang'
+import en from "../translate/en.json"
+import fa from "../translate/fa.json"
+import { useState } from "react";
+
+setDefaultLanguage('en')
+setDefaultTranslations({ en, fa })
 
 const Layout = () => {
+
+    const [lang, setLang] = useState(false)
+
     return (
         <>
             <Header />
-            <main className="w-full max-w-screen-2xl mx-auto flex justify-center flex-wrap gap-y-20">
-                <Outlet />
+            <main className="w-full mx-auto flex justify-center flex-wrap gap-y-10 lg:gap-y-20 dark:bg-[#2B2B2B] pb-10 lg:pb-20">
+                <div className="w-12 lg:w-16 z-30 h-fit  rounded-full bg-slate-500 p-1 fixed left-2 sm:left-16 bottom-2 sm:bottom-16 flex flex-wrap">
+                    <DarkBtn />
+                    <Translate lang={lang} setLang={setLang} />
+                </div>
+                <Outlet context={lang} />
             </main>
             <Footer />
         </>
